6978cc16fcaec873261dc12e6ddb37773615db04
ZiadAhmed9 <zeyadahmed0@gmail.com>
2023-08-16 21:31:51 +0300

    makefile task added

---
+++ Makefile task/head.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+void func()
+{
+  std::cout<<"hi i'm function from head.c"<<std::endl;	
+}
---
+++ Makefile task/main.cpp
@@ -0,0 +1,11 @@
+
+#include <iostream>
+#include "head.hpp"
+using namespace std;
+
+int main()
+{
+ cout <<"Hello this is ziad from main.c"<<endl;
+ func();
+return 0;
+}
